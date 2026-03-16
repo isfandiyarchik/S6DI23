6a4fe938dd3c90d5414856a7c57efcfb84f93c1f
Isfandiyarchik <r.isfan@mail.ru>
2026-03-16 11:11:08 +0500

    main.py

--- main.py
+++ main.py
@@ -230,9 +230,10 @@ def check_access_cb(func):
         if not is_admin(uid) and not is_authorized(uid):
             bot.answer_callback_query(call.id, "⛔ Рұхсат жоқ!")
             return
-    if not is_admin(call.from_user.id):
-       _update_last_active(call.from_user.id)
-    return func(call)
+        if not is_admin(uid):
+            _update_last_active(uid)
+        return func(call)
+    return wrapper
 
 # FIX 2: next_step handler-лар үшін access check хелпері
 def admin_only_step(func):
